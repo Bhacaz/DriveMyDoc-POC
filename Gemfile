702acd8951b969822aa5ba0ccf3ba36d70152e49
Jean-Francis Bastien <bhacaz@gmail.com>
2019-02-21 23:49:39 -0500

    Added hierarchy service and view

--- Gemfile
+++ Gemfile
@@ -58,6 +58,8 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
 
   gem 'rubocop'
+
+  gem 'awesome_print'
 end
 
 group :test do
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,12 @@
 class User < ApplicationRecord
   def self.from_omniauth(auth)
     # Creates a new user only if it doesn't exist
-    where(email: auth.info.email).first_or_initialize do |user|
+    find_or_initialize_by(email: auth.info.email) do |user|
       user.name = auth.info.name
       user.email = auth.info.email
       user.token = auth.credentials.token
       user.refresh_token = auth.credentials.refresh_token
-      user.expires_at = Time.at.utc(auth.credentials.expires_at)
+      user.expires_at = auth.credentials.expires_at
       user.save!
     end
   end
