1f23374d885af1f0ca26fa4e5d1a579f502ac10a
Jean-Francis Bastien <bhacaz@gmail.com>
2019-02-21 21:50:08 -0500

    Added session controller and login page

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,20 +8,8 @@ class User < ApplicationRecord
       user.email = auth.info.email
       user.token = auth.credentials.token
       user.refresh_token = auth.credentials.refresh_token
-      user.expires_at = auth.credentials.expires_at
+      user.expires_at = Time.at.utc(auth.credentials.expires_at)
       user.save!
     end
-  end
-
-  def google_authorization
-    scope = 'userinfo.email, drive'
-    Google::Auth::UserRefreshCredentials.new(
-      client_id: ENV['GOOGLE_CLIENT_ID'],
-      client_secret: ENV['GOOGLE_CLIENT_SECRET'],
-      scope: scope,
-      access_token: token,
-      refresh_token: refresh_token,
-      expires_at: expires_at
-    )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'auth/:provider/callback', to: 'sessions#googleAuth'
+  get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+  resources :sessions, only: %i[create destroy]
+
   get 'drive', to: 'drive#index'
   get 'drive/:id', to: 'drive#show'
+
+  resource :home, only: [:show]
+  root to: 'sessions#show'
 end
